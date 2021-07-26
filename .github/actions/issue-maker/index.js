3d4bf888b8a8217a78830bba27287a8fb23034da
Chris Millar <cmillar@adobe.com>
2021-07-26 14:06:17 -0600

    commit comment attempt 1

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -3,18 +3,26 @@ const github = require("@actions/github");
 
 async function run() {
   try {
-    const issueTitle = core.getInput("issue-title");
-    const jokeBody = core.getInput("joke");
     const token = core.getInput("repo-token");
 
     const octokit = new github.getOctokit(token);
 
-    const newIssue = octokit.rest.issues.create({
-      repo: github.context.repo.repo,
-      owner: github.context.repo.owner,
-      title: issueTitle,
-      body: jokeBody
+    const issue_number = context.payload.pull_request.number;
+    const owner = context.github.repository_owner;
+    const repo = context.payload.repository.name;
+    const newComment = octokit.issues.createComment({
+      owner,
+      repo,
+      issue_number,
+      body,
     });
+
+    // const newIssue = octokit.rest.issues.create({
+    //   repo: github.context.repo.repo,
+    //   owner: github.context.repo.owner,
+    //   title: issueTitle,
+    //   body: jokeBody
+    // });
   } catch (error) {
     core.setFailed(error.message);
   }
