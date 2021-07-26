7319792da2a0df52ba3ff8cb0cd5f716fda6e0b2
Chris Millar <cmillar@adobe.com>
2021-07-26 14:21:21 -0600

    t

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -4,8 +4,13 @@ const github = require("@actions/github");
 async function run() {
   try {
     const token = core.getInput("repo-token");
-
     const octokit = new github.getOctokit(token);
+
+    const body = `![Lighthouse](https://img.shields.io/badge/LH-100-brightgreen?style=flat-square) 
+                  ![First Contentful Pain](https://img.shields.io/badge/FCP-0.8s-brightgreen?style=flat-square) 
+                  ![Largest Contentful Pain](https://img.shields.io/badge/LCP-1.4s-brightgreen?style=flat-square) 
+                  ![Total Blocking Time](https://img.shields.io/badge/TBT-90ms-brightgreen?style=flat-square) 
+                  ![Cumulative Layout Shift](https://img.shields.io/badge/CLS-0s-brightgreen?style=flat-square)`;
 
     const issue_number = github.context.payload.pull_request.number;
     const owner = github.context.repo.owner;
